d5e929168e50812cbbd0808c16bb9f757359b87c
Joan <joanfcomontenegro@gmail.com>
2023-11-27 09:47:25 -0500

    aver

--- src/js/crude/crudecancelar.js
+++ src/js/crude/crudecancelar.js
@@ -9,11 +9,16 @@ document.addEventListener('click', (e) => {
         httpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
         httpRequest.onload = () =>{
         console.log(httpRequest.status);
+        if(httpRequest.status == 200)
+        {
+            window.location='../home.php?pg=10';
+        }
+        
         }
         // httpRequest.send(JSON.stringify(payload));
             
         httpRequest.send(`controller=cancelar&id_suscripcion=${encodeURIComponent(e.target.id.split('-')[1])}`);
-        window.location='../home.php?pg=10';
+        
        
 
     }
